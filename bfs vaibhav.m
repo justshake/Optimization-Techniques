092f2a4061304e7bd490819ef2d675e1beb0289c
justshake <161639604+justshake@users.noreply.github.com>
2024-02-29 00:47:20 +0530

    Create bfs vaibhav.m

---
+++ bfs vaibhav.m
@@ -0,0 +1,26 @@
+clc 
+clear all;
+A=[3 -1 1 1 0 0;
+   -1 2 0 0 1 0;
+   -4 3 8 0 0 1];
+b=[7;6;10];
+C=[-1 3 -3 0 0 0];
+[m,n]=size(A)
+p=nchoosek(n,m);
+a=1:1:n
+c=nchoosek(a,m);
+sol=[];
+for i=1:p
+    x=zeros(1,n)
+    B=A(:,c(i,:))  
+        X(i,:)=B\b
+        if X(i,:)>0
+            x(c(i,:))=X(i,:)
+            sol=[sol; x]
+        end        
+    end
+end
+
+[s,f]=max(C*sol');
+sol(f,:)
+s
